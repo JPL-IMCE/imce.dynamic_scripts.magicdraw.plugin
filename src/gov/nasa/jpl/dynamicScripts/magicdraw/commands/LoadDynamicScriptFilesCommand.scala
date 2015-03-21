8148c352f25fd978fa862a910f20d7b14c72bdc9
Nicolas Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-03-21 10:23:03 -0700

    SSCAES-1043

--- src/gov/nasa/jpl/dynamicScripts/magicdraw/commands/LoadDynamicScriptFilesCommand.scala
+++ src/gov/nasa/jpl/dynamicScripts/magicdraw/commands/LoadDynamicScriptFilesCommand.scala
@@ -25,28 +25,41 @@ import com.nomagic.magicdraw.core.Application
 import com.nomagic.magicdraw.utils.MDLog
 import scala.util.Try
 import scala.util.Success
+import java.nio.file.Paths
+import gov.nasa.jpl.dynamicScripts.magicdraw.utils.MDUML
 
 /**
  * @author Nicolas F. Rouquette (JPL)
  */
-class LoadDynamicScriptFilesCommand( refresh: () => Unit) extends RunnableWithProgress {
+class LoadDynamicScriptFilesCommand( refresh: () => Unit ) extends RunnableWithProgress {
 
   override def run( progressStatus: ProgressStatus ): Unit = {
     val p = DynamicScriptsPlugin.getInstance()
     val files = p.getDynamicScriptsOptions().getDynamicScriptConfigurationFiles()
-    val message = p.updateRegistryForConfigurationFiles( files ) match {
-      case None =>       
-        refresh()
-        "\nSuccess"
-      case Some( m ) =>
-        "\n" + m
-    }
-
     val guiLog = Application.getInstance().getGUILog()
-    guiLog.log( "=============================" )
-    guiLog.log( s"Reloaded ${files.size} dynamic script files:" )
-    files.foreach { f => guiLog.log( "\n => " + f ) }
-    guiLog.log( message )
-    guiLog.log( "=============================" )
+    try {
+      val installRoot = Paths.get( MDUML.getInstallRoot )
+      val paths = ( for {
+        file <- files
+        path = Paths.get( file )
+        dsPath = if ( path.isAbsolute ) path else installRoot.resolve( path )
+      } yield dsPath.toFile.getAbsolutePath ).toList
+      val message = p.updateRegistryForConfigurationFiles( paths ) match {
+        case None =>
+          refresh()
+          "\nSuccess"
+        case Some( m ) =>
+          "\n"+m
+      }
+
+      guiLog.log( "=============================" )
+      guiLog.log( s"Reloaded ${paths.size} dynamic script files:" )
+      paths.foreach { f => guiLog.log( "\n => "+f ) }
+      guiLog.log( message )
+      guiLog.log( "=============================" )
+    } catch {
+      case t: Throwable =>
+        guiLog.showError( s"Failed to load dynamicScript file", t )
+    }
   }
 }
